8486b2c5c4b6ce3cb60a2aecc0737cecd427a986
Pythonic671 <ezrakipngetich628@gmail.com>
2023-06-16 14:41:07 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints numbers from 0 to 9
+ * main - prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
  *
- * Returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
