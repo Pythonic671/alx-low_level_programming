fdb8456b7bc114023126b027c5cad37610a6019d
Pythonic671 <ezrakipngetich628@gmail.com>
2023-06-20 16:11:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main entry point of my code
+ * main: Entry point of my code,
  *
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
