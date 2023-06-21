04e182a99d3f3a924276342c7f9a3ea7d960c987
Pythonic671 <ezrakipngetich628@gmail.com>
2023-06-21 10:17:14 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,14 +6,14 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			k = j*i;
-			if(j == 0)
+			k = j * i;
+			if (j == 0)
 			{
-				_putchar (k + '0');
+				_putchar(k + '0');
 			}
 			if (k < 10 && j != 0)
 			{
@@ -24,10 +24,10 @@ void times_table(void)
 			}
 			else if (k >= 10)
 			{
-				_putchar (',');
-				_putchar (' ');
-				_putchar ((k / 10) + '0');
-				_putchar ((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
