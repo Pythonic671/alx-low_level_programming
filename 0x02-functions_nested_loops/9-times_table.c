9c8bd54f9265e0e556de43a6fb4cd356cdc68031
Pythonic671 <ezrakipngetich628@gmail.com>
2023-06-21 08:40:55 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 			k = j*i;
 			if(j == 0)
 			{
-				_putchar(k + '0');
+				_putchar (k + '0');
 			}
 			if (k < 10 && j != 0)
 			{
@@ -24,10 +24,10 @@ void times_table(void)
 			}
 			else if (k >= 10)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar (',');
+				_putchar (' ');
+				_putchar ((k / 10) + '0');
+				_putchar ((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
