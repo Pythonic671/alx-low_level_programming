0fdfe98d64471ba72418cd96a67a8e79df1a21da
Pythonic671 <ezrakipngetich628@gmail.com>
2023-06-21 09:32:24 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
  * print_to_98 -prints all natural numbers from n to 98,
  * followed by a new line
- * n: print from this number
+ * @n: print from this number
  */
 void print_to_98(int n)
 {
@@ -25,11 +25,8 @@ void print_to_98(int n)
 		{
 			if (j != 98)
 				printf("%d, ", j);
-			else if(j == 98)
+			else if (j == 98)
 				printf("%d\n", j);
 		}
 	}
-}
-		
-				
-	
+}	
