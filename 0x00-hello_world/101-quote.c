097fd659375cf3f1c6d0b44c37868eeded81af1a
Pythonic671 <ezrakipngetich628@gmail.com>
2023-06-24 01:41:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,20 @@
+#include<stdio.h>
 #include<unistd.h>
+
 /**
- * main - Entry point
+ * main - run all functions
  *
- * Return: Always 1 (Success)
+ * Description: Change standard error message
+ * Return: Return 1 and exit
  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" 
-	- Dora Korpar,2015-10-19\n", 59);
+	int l;
 
+	l =
+	  sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
 	return (1);
 }
