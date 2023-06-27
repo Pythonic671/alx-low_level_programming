76622c83a21e71a3b6de3df38d759dfbe6936a87
Pythonic671 <ezrakipngetich628@gmail.com>
2023-06-26 17:24:49 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,3 +9,15 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
+int main(void)
+{
+	int n;
+	int *p;
+
+	p = &n;
+	n = 402;
+	_putchar("The value of 'n' before the call: %d\n", n);
+	reset_to_98(n);
+	_putchar("The value of 'n' after the call: %d\n", n);
+	return (0);
+}
